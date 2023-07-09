0468793d55f492469223430f4db0f92b4b340d5b
Ian Wigley <ian.wigley@outlook.com>
2023-07-09 18:31:42 +0100

    Update main.go

--- SmallExamples/main.go
+++ SmallExamples/main.go
@@ -7,19 +7,19 @@ import (
 	"strconv"
 )
 
-//export add_test
-func add_test(value int) int {
+//export addTest
+func addTest(value int) int {
 	var stringValue string = strconv.Itoa(value)
 	print("\nAdding the value supplied from Python : ", stringValue)
 	print("\nTo 42 from go ... equals -> ")
 	return value + 42
 }
 
-//export string_test
-func string_test(text *C.char) *C.char {
-	var return_string string = "Hello from Go !!"
-	return_string += "\n" + C.GoString(text)
-	return C.CString(return_string)
+//export stringTest
+func stringTest(text *C.char) *C.char {
+	var returnString string = "Hello from Go !!"
+	returnString += "\n" + C.GoString(text)
+	return C.CString(returnString)
 }
 
 func main() {}
