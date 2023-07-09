acf16135ae683344139c3cd92bb85d902f7ebfe7
Ian Wigley <ian.wigley@outlook.com>
2023-07-09 18:31:46 +0100

    Update PythonInterOp.py

--- SmallExamples/PythonInterOp.py
+++ SmallExamples/PythonInterOp.py
@@ -4,9 +4,9 @@ import os
 ## Python <-> Go interop example
 
 go_object = ctypes.cdll.LoadLibrary(os.getcwd() + '/_go-add.so')
-print(go_object.add_test(10))
+print(go_object.addTest(10))
 
-string_test = go_object.string_test
+string_test = go_object.stringTest
 string_test.restype = ctypes.c_void_p
 ptr = string_test('Hello from Python !!'.encode('utf-8'))
 out = ctypes.string_at(ptr)
