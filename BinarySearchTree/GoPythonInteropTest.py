78c5d8c5d6f9b00606937aad27c8d3016c3371bc
Ian Wigley <ian.wigley@outlook.com>
2023-07-09 18:31:22 +0100

    Update GoPythonInteropTest.py

--- BinarySearchTree/GoPythonInteropTest.py
+++ BinarySearchTree/GoPythonInteropTest.py
@@ -14,11 +14,11 @@ from ctypes import *
 so = ctypes.cdll.LoadLibrary('./_interop.so')
 
 pyarr = [1,2,3,5,7,8,55,10,100,101,102,103]
-so.binary_tree.argtypes = [c_int * len(pyarr)]
-so.binary_tree.restype = ctypes.c_void_p
+so.binaryTree.argtypes = [c_int * len(pyarr)]
+so.binaryTree.restype = ctypes.c_void_p
 
 # Make C array from py array
 arr = (c_int * len(pyarr))(*pyarr)
-ptr = so.binary_tree(arr, len(arr), 2)
+ptr = so.binaryTree(arr, len(arr), 2)
 out = ctypes.string_at(ptr)
 print('Result from Go! : ' + out.decode('utf-8'))
